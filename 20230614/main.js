96de2ff2b26f6e92f9560cc01f60b12a1eaf71a8
Nick Sotula <so2niko@gmail.com>
2023-06-14 17:51:36 +0000

    add filters

--- 20230614/main.js
+++ 20230614/main.js
@@ -1,9 +1,33 @@
 const URL_HP = 'https://hp-api.onrender.com/api/characters';
 const tableBodyDOM = document.querySelector('#hp-characters-table-body');
+const filtersBtns = document.querySelectorAll('.btn-filters');
+let DATA = null;
+
+filtersBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn.dataset.facultet);
+
+        if(btn.dataset.filter === 'All'){
+            renderTable(DATA);
+            return;  
+        } 
+        const choice = btn.dataset.filter === 'None' ? '' : btn.dataset.filter;
+        const filteredArr = DATA.filter(ch => ch.house === choice);
+        renderTable(filteredArr);
+
+        // Додаткове
+        btn.innerText = `${ btn.dataset.filter } ${ filteredArr.length }`;
+        // Кінець додаткового
+        // console.log(DATA);
+    });
+});
  
 fetch(URL_HP)
     .then(r => r.json())
-    .then(renderTable);
+    .then((d) => {
+        DATA = d;
+        renderTable(DATA);
+    });
 
 function renderTable(d){
     const rows = d.map(getCharacterRow);
@@ -20,3 +44,4 @@ function getCharacterRow({ name, house, gender, actor, wizard }){
     <td>${ Boolean(wizard) ? '🧙' : 'other' }</td>
     </tr>`;
 }
+
