643cbfab5a648119ce1dbe559fe50a46d40f4480
Nick Sotula <so2niko@gmail.com>
2023-06-19 17:32:15 +0000

    feat: add filter

--- 20230614/main.js
+++ 20230614/main.js
@@ -1,31 +1,42 @@
 const URL_HP = 'https://hp-api.onrender.com/api/characters';
 const tableBodyDOM = document.querySelector('#hp-characters-table-body');
-const filtersBtns = document.querySelectorAll('.btn-filters');
+const selectHouses = document.querySelector('#sel_houses');
+const headerName = document.querySelector('.header_name');
+let isNameASCsort = false;
 let DATA = null;
+let FILTERED_DATA = null;
 
-filtersBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        console.log(btn.dataset.facultet);
+selectHouses.addEventListener('input', (ev) => {
+    const house = ev.target.value;
 
-        if(btn.dataset.filter === 'All'){
-            renderTable(DATA);
-            return;  
-        } 
-        const choice = btn.dataset.filter === 'None' ? '' : btn.dataset.filter;
-        const filteredArr = DATA.filter(ch => ch.house === choice);
-        renderTable(filteredArr);
-
-        // Додаткове
-        btn.innerText = `${ btn.dataset.filter } ${ filteredArr.length }`;
-        // Кінець додаткового
-        // console.log(DATA);
-    });
+    if(house === 'All'){
+        FILTERED_DATA = DATA;
+        renderTable(DATA);
+        return;  
+    } 
+    const choice = house === 'None' ? '' : house;
+    const filteredArr = DATA.filter(ch => ch.house === choice);
+    FILTERED_DATA = filteredArr;
+    renderTable(filteredArr);
 });
- 
+
+headerName.addEventListener('click', () => {
+    isNameASCsort = !isNameASCsort;
+    const sortASC = (a, b) => a.name > b.name ? 1 : -1;
+    const sortDESC = (a, b) => b.name > a.name ? 1 : -1;
+
+    const sortF = isNameASCsort ? sortASC : sortDESC;
+
+    FILTERED_DATA.sort(sortF);
+    
+    renderTable(FILTERED_DATA);
+});
+
 fetch(URL_HP)
     .then(r => r.json())
     .then((d) => {
         DATA = d;
+        FILTERED_DATA = [...d];
         renderTable(DATA);
     });
 
