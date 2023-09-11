bfeca8fd06d6d0967c92c6b7a36b3f7c9861e54b
Nick Sotula <so2niko@gmail.com>
2023-09-11 17:35:01 +0000

    add hw

---
+++ 20230911-jest/calculator.js
@@ -0,0 +1,41 @@
+function calculator(op, ...args){
+    let result = 0;
+    switch(op){
+        case '/':
+        case ':' : {
+            break;
+        }
+        case '*' : {
+            break;
+        }
+        case '+' : {
+            break;
+        }
+        case '-' : {
+            break;
+        }
+        default:{
+
+        }
+    }
+
+    return result;
+}
+
+/*
+Написати тести на функції:
+- калькулятор (calculator.js , calculator.test.js)
+- ділення (divide.js , divide.test.js)
+- множення (multi.js , multi.test.js)
+- додавання (plus.js , plus.test.js)
+- віднімання (minus.js , minus.test.js)
+
+
+Перевіряти на:
+- позитивний результат
+- крайні можливі значення
+- на нуль
+- на неправильну кількість аргументів
+- на неправильний тип аргументів (рядок, логічний, об`єкт, масив, тощо)
+
+*/
